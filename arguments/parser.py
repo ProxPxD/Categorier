1411d68bfc77dbbc1a118f62e5eb20e7a47e1463
proxpxd <piotr10tutek@poczta.onet.pl>
2022-09-12 22:20:54 +0200

    add skeleton of parser and arguments logic

---
+++ arguments/parser.py
@@ -0,0 +1,8 @@
+def parse(args: list[str]):
+    pass
+
+
+class Arguments:
+
+    def __init__(self, word_or_phrase: str, categories: list[str]):  #TODO think of a shorter name for "word_or_phrase"
+        pass
