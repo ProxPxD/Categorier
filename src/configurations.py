ebc315e639d021416d56a5846f9fadd91f855e69
proxpxd <piotr10tutek@poczta.onet.pl>
2022-09-15 12:52:28 +0200

    implement logic to make a separate directory for tests

--- src/configurations.py
+++ src/configurations.py
@@ -9,13 +9,21 @@ from ideasList import IdeasList
 @dataclass
 class Saved:
     RESOURCES = Path(__file__).parent / 'resources'
-    IDEAS_LIST_PATH = RESOURCES / 'ideas.pickle'
-    CATEGORIES_LIST_PATH = RESOURCES / 'categories.pickle'
+    DATA_DIR = RESOURCES / 'data'
+    IDEAS_NAME = 'ideas.pickle'
+    CATEGORIES_NAME = 'categories.pickle'
+    IDEAS_PATH = DATA_DIR / IDEAS_NAME
+    CATEGORIES_PATH = DATA_DIR / CATEGORIES_NAME
+
+    TESTS_DIR = RESOURCES / 'tests'
+    CATEGORIES_TESTS_PATH = TESTS_DIR / CATEGORIES_NAME
+    IDEAS_TESTS_PATH = TESTS_DIR / IDEAS_NAME
 
 
-def load_list(path: Path):
+def load_list(path: Path, test_mode=False):
     if not path.exists():
-       save_default_list(path)
+        _create_directories(test_mode)
+        _save_default_list(path)
 
     with open(path, 'rb') as f:
         return pickle.load(f)
@@ -27,12 +35,23 @@ def save_list(list_to_save: Any, path: Path):
 
 
 def get_default_list(path: Path):
-    if path == Saved.IDEAS_LIST_PATH:
+    if Saved.IDEAS_NAME in path.name:
         return IdeasList()
-    elif path == Saved.CATEGORIES_LIST_PATH:
+    elif Saved.CATEGORIES_NAME in path.name:
         return []
     return []
 
 
-def save_default_list(path: Path):
+def _save_default_list(path: Path, test_mode=False):
     save_list(get_default_list(path), path)
+
+
+def _create_directories(test_mode=False):
+    dirs = (Saved.RESOURCES, Saved.DATA_DIR) if not test_mode else (Saved.RESOURCES, Saved.TESTS_DIR)
+    for path in dirs:
+        if not path.exists():
+            path.mkdir()
+
+
+def remove_file(path: Path):
+    path.unlink()
