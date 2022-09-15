2ba88a6cacdc7e3d14051f297599dab67d65e4c3
proxpxd <piotr10tutek@poczta.onet.pl>
2022-09-15 12:49:23 +0200

    add failing tests for ideas

---
+++ tests/ideasTest.py
@@ -0,0 +1,17 @@
+import unittest
+
+
+class IdeasTest(unittest.TestCase):
+
+    def test_default_add_idea_mode(self):
+        self.fail()
+
+    def test_add_idea_with_descriptions(self):
+        self.fail()
+
+    def test_add_idea_with_categories(self):
+        self.fail()
+
+    def test_add_idea_with_categories_and_descriptions(self):
+        self.fail()
+        
