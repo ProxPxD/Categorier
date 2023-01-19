8675aa01afa30c0a24cb995194003681d70925b6
ProxPxD <piotr10tutek@poczta.onet.pl>
2023-01-19 13:04:39 +0100

    fix test

--- tests/searchTest.py
+++ tests/searchTest.py
@@ -16,10 +16,10 @@ class SearchTest(AbstractTest):
 	@parameterized.expand([
 		('by_value_in_arguments', ['Poland', 'Portugal', 'Czechia'], 'Europe', f'{K.BY} continent'),
 		('by_value_in_by', ['Poland', 'Portugal', 'Czechia'], '', f'{K.BY} continent Europe'),
-		('by_values_in_arguments', ['Czechia'], 'Europe Cz', f'{K.BY} continent and memo'),
-		('by_values_in_arguments', ['Czechia', 'Chile'], 'Europe Ch', f'{K.BY} continent or memo'),
-		('by_values_in_by', ['Poland', 'Portugal'], '', f'{K.BY} continent Europe and memo P'),
-		('by_values_in_by', ['Poland', 'Portugal', 'Peru'], '', f'{K.BY} continent Europe or memo P'),
+		('by_values_in_arguments_and', ['Czechia'], 'Europe Cz', f'{K.BY} continent and memo'),
+		('by_values_in_arguments_or', ['Czechia', 'Chile', 'Poland', 'Portugal',], 'Europe Ch', f'{K.BY} continent or memo'),
+		('by_values_in_by_and', ['Poland', 'Portugal'], '', f'{K.BY} continent Europe and memo P'),
+		('by_values_in_by_or', ['Poland', 'Czechia', 'Portugal', 'Peru'], '', f'{K.BY} continent Europe or memo P'),
 		('by_value_not_set', ['Chile'], '', f'{K.BY} continent None'),
 		('by_name_explicit', ['Czechia', 'Colombia', 'Chile'], 'C', ''),
 		('by_name_implicit', ['Czechia', 'Colombia', 'Chile'], 'C', f'{K.BY} {K.MEMO}'),
@@ -35,7 +35,7 @@ class SearchTest(AbstractTest):
 		lines = SmartList()
 		self.cli.set_out_stream(lines.__iadd__)
 		self.cli.parse(f'm {K.SEARCH} {to_search} {search_by}')
-		e_results = [f'{i+1}) {line}' for i, line in enumerate(e_results)]
+		# e_results = [f'{i+1}) {line}' for i, line in enumerate(e_results)]
 
 		self.assertCountEqual(e_results, lines)
 
